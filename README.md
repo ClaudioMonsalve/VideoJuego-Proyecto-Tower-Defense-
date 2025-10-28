58c0453fcec6f47388a62341faa0bdad404469ce
Mauro <lainezmauro0@gmail.com>
2025-10-28 16:38:05 -0300

    Se agrega el suelo realizado en blender

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
--Actualice para que tenga 2 caminos.
-
--Tambien agrege un enemigo(solo duplique el enemigo que habia y le cambie el 
-color).
-
--Y cambie el scrpit para generar a los enemigos en los 2 caminos, la generacion
-es "random", ya que hay una probabilidad (que se cambia en el script) para el 
-segundo enemigo, y se generana un random (entre 0 y 1) que se ocupa para la 
-ver cual enemigo generar.
+A
--- scripts/creep_manager.gd
+++ scripts/creep_manager.gd
@@ -3,7 +3,6 @@ extends Node3D
 @export var enemy1 = preload("res://scenes/test_enemy.tscn")
 @export var enemy2 = preload("res://scenes/test_enemy2.tscn") 
 
-
 @export var enemy2_spawn_chance: float = 0.5
 
 @export var spawn_radius: float = 1.5
@@ -11,17 +10,21 @@ extends Node3D
 @onready var pathHolder = $"../Map/Paths"
 
 var waveDelay = 5.0
-var spanwDelay = 0.1
+var spanwDelay = 0.1 # (Tu script original dice "spanwDelay", lo mantengo)
 
-# --- ESTRUCTURA DE OLEADAS  ---
+# --- ESTRUCTURA DE OLEADAS (MODIFICADA PARA 6 CAMINOS) ---
 var waves = [
-	[5, 7],  # Oleada 1: 5 enemigos en total por el camino 0, 7 por el camino 1
-	[10, 14]   # Oleada 2: 10 enemigos en total por el camino 0 y 14 por el 1
+	# Formato: [Camino 0, Camino 1, Camino 2, Camino 3, Camino 4, Camino 5]
+	[5, 7, 0, 0, 0, 0],  # Oleada 1: 5 enemigos por el camino 0, 7 por el 1. El resto 0.
+	[10, 14, 5, 5, 0, 0], # Oleada 2: 10 por el 0, 14 por el 1, 5 por el 2, 5 por el 3.
+	[20, 20, 10, 10, 5, 5]  # Oleada 3: Enemigos en todos los caminos.
 ]
 
 
 func waveManager():
 	for wave in waves:
+		# Esta línea lee el tamaño de la oleada (ahora 6)
+		# por lo que el bucle se ejecutará 6 veces (de 0 a 5).
 		for path_index in range(wave.size()):
 			var amount = wave[path_index] # Cantidad total para este camino
 			
@@ -37,6 +40,7 @@ func waveManager():
 				var creep = enemy_to_spawn.instantiate()
 				# ------------------------------------
 				
+				# Esta línea ahora llamará a get_child(0), get_child(1), ..., get_child(5)
 				pathHolder.get_child(path_index).add_child(creep)
 				
 				var random_offset = Vector3(randf_range(-spawn_radius, spawn_radius), 
